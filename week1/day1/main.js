785faa0ab41b253ffc1f97b81faa73514d482e5d
nek-yt <googllgoog36@gmail.com>
2026-02-19 15:12:19 +0500

    week1-day1

---
+++ week1/day1/main.js
@@ -0,0 +1,53 @@
+let box = document.querySelector(".cont")
+box.style.margin = "auto"
+box.style.marginTop = "50px"
+box.style.padding = "100px"
+box.style.border = "1px solid gray"
+box.style.width = "300px"
+box.style.height = "500px"
+box.style.fontFamily = "Sans-serif"
+box.style.textAlign = "center"
+box.style.background = "white"
+console.log(box);
+
+let box2 = document.querySelector(".d1")
+box2.style.background = "blue"
+box2.style.width = "100px"
+box2.style.height = "120px"
+box2.style.boxShadow = "2px 2px 10px gray"
+console.log(box2);
+
+let box3 = document.querySelector(".d2")
+box3.style.background = "blue"
+box3.style.width = "100px"
+box3.style.height = "120px"
+box3.style.boxShadow = "2px 2px 10px gray"
+box3.style.marginLeft = "165px"
+console.log(box3);
+
+let txt = document.querySelector(".txt")
+txt.style.marginTop = "100px"
+console.log(txt);
+
+let img = document.querySelector(".img")
+img.style.width = "200px"
+console.log(img);
+
+let bx1 = document.querySelector(".c1")
+bx1.style.background = "blue"
+bx1.style.width = "200px"
+bx1.style.height = "200px"
+bx1.style.boxShadow = "2px 2px 10px gray"
+bx1.style.borderRadius = "10px"
+console.log(bx1);
+ 
+let bx2 = document.querySelector(".c2")
+bx2.style.background = "blue"
+bx2.style.width = "200px"
+bx2.style.height = "200px"
+bx2.style.boxShadow = "2px 2px 10px gray"
+bx2.style.borderRadius = "10px"
+console.log(bx2);
+
+let tc = document.querySelector(".txtCont")
+console.log(tc);
